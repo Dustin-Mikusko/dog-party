ebd6c162a9a82c580add4d21db2a66c7ded0e069
Dustin Mikusko <dustinjm713@yahoo.com>
2019-08-17 01:01:47 -0600

    Begin iteration 3

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ button.addEventListener('click', function() {
   dogName.innerText = ` ${nameInput.value}`;
   nameInput.value = '';
 });
+
+var button1 = document.querySelector('#button1');
+var button2 = document.querySelector('#button2');
+var button3 = document.querySelector('#button3');
+var paraHow = document.querySelector('#para-how');
+var paraWhat = document.querySelector('#para-what');
+var paraFacts = document.querySelector('#para-facts');
+
+button1.addEventListener('click', function() {
+   if (paraHow.hidden === false) {
+     paraHow.hidden = true;
+   } else {
+     paraHow.hidden = false;
+   }
+});
+
+button2.addEventListener('click', function() {
+   if (paraWhat.hidden === false) {
+     paraWhat.hidden = true;
+   } else {
+     paraWhat.hidden = false;
+   }
+});
+
+button3.addEventListener('click', function() {
+   if (paraFacts.hidden === false) {
+     paraFacts.hidden = true;
+   } else {
+     paraFacts.hidden = false;
+   }
+});
