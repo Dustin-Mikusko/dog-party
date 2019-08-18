63b19bb41eb97ff199727c8887c211cdf5b6f277
Dustin Mikusko <dustinjm713@yahoo.com>
2019-08-18 17:22:49 -0600

    Update JS file. Add tooltip hover function

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-var button = document.querySelector('#name-change-button');
+var nameButton = document.querySelector('#name-change-button');
 var nameInput = document.getElementById('dog-name-box');
-
-
-button.addEventListener('click', function() {
-  dogName.innerText = ` ${nameInput.value}`;
-  nameInput.value = '';
-});
-
+var columnButton = document.querySelector('#column-change-button');
+var columnInput = document.getElementById('column-change-box')
 var button1 = document.querySelector('#button1');
 var button2 = document.querySelector('#button2');
 var button3 = document.querySelector('#button3');
 var paraHow = document.querySelector('#para-how');
 var paraWhat = document.querySelector('#para-what');
 var paraFacts = document.querySelector('#para-facts');
+var dropDown = document.querySelector('select');
+
+nameButton.addEventListener('click', function() {
+  dogName.innerText = ` ${nameInput.value}`;
+  nameInput.value = '';
+});
+
+columnButton.addEventListener('click', function() {
+  if (dropDown.value === "howToDog") {
+    paraHow.innerText = columnInput.value;
+    columnInput.value = '';
+  } if (dropDown.value === "whatIsDogs") {
+    paraWhat.innerText = columnInput.value;
+    columnInput.value = '';
+  } if (dropDown.value === "factsFromDog") {
+    paraFacts.innerText = columnInput.value;
+    columnInput.value = '';
+  }
+})
 
 button1.addEventListener('click', function() {
    if (paraHow.hidden === false) {
